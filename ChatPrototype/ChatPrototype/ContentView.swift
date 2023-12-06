bca664ea15133aae13bfb39f41a632d8fef3fd9d
Shihang Wei <wei13904639403@gmail.com>
2023-12-05 23:00:12 -0500

    explore Xcode

--- ChatPrototype/ChatPrototype/ContentView.swift
+++ ChatPrototype/ChatPrototype/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hey")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+            Text("I'm Shihang")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
         }
         .padding()
     }
