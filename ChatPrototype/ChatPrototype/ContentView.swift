cfc8179fd125882696b554dfbdfdd3adcabfdba7
Shihang Wei <wei13904639403@gmail.com>
2023-12-05 23:12:34 -0500

    weather practice created

--- ChatPrototype/ChatPrototype/ContentView.swift
+++ ChatPrototype/ChatPrototype/ContentView.swift
@@ -9,10 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack (spacing: 30){
             Text("Hey")
                 .padding()
                 .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
             Text("I'm Shihang")
                 .padding()
                 .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Shihang Wei on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
