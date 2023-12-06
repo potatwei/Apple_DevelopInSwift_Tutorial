40d9ceb27576032b0160d4a01d6c0e9eb23ce4cb
Shihang Wei <wei13904639403@gmail.com>
2023-12-06 00:29:55 -0500

    finished weather practice

--- WeatherForecast/WeatherForecast/ContentView.swift
+++ WeatherForecast/WeatherForecast/ContentView.swift
@@ -9,16 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            DayForcast(day: "Mon", isRainy: false, high: 70, low: 50)
+            
+            DayForcast(day: "Tue", isRainy: true, high: 60, low: 40)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DayForcast: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text(day)
+                .font(.headline)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .font(.largeTitle)
+                .padding(5)
+            Text("High: \(high)º")
+                .fontWeight(.semibold)
+            Text("Low: \(low)º")
+                .fontWeight(.medium)
+                .foregroundStyle(Color.secondary)
+        }
+        .padding()
+    }
 }
