964d554e0d2664fd414b2c9f33e5af2367b1f022
Shihang Wei <wei13904639403@gmail.com>
2023-12-11 00:15:55 -0500

    added JSON decoding

---
+++ DecodingStructuredJson/StartingProject/Earthquakes/Models/QuakeError.swift
@@ -0,0 +1,25 @@
+//
+//  QuakeError.swift
+//  Earthquakes-iOS
+//
+//  Created by Shihang Wei on 12/10/23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import Foundation
+
+enum QuakeError: Error {
+    case missingData
+}
+
+extension QuakeError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .missingData:
+            return NSLocalizedString(
+                "Found and will discard a quake missing a valid code, magnitude, place, or time.",
+                comment: ""
+            )
+        }
+    }
+}
